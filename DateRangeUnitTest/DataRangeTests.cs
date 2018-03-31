f0e3db4357a1eac5ab482670033d6a5937f98fb7
GillinedUp <myrreah@gmail.com>
2018-03-31 14:41:52 +0300

    added tests

--- DateRangeUnitTest/DataRangeTests.cs
+++ DateRangeUnitTest/DataRangeTests.cs
@@ -25,5 +25,94 @@ namespace DateRangeUnitTest
             Assert.AreEqual(date1Expected, date1Actual);
             Assert.AreEqual(date2Expected, date2Actual);
         }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestForExceptionInValidateDates()
+        {
+            // arrange
+            DateRange dateRange = new DateRange();
+            var date1 = new DateTime(2017, 2, 2);
+            var date2 = new DateTime(2017, 2, 1);
+
+            //act
+            dateRange.ValidateDates(date1, date2);
+        }
+
+        [TestMethod]
+        public void TestForNoExceptionForEqualDates()
+        {
+            // arrange
+            DateRange dateRange = new DateRange();
+            var date1 = new DateTime(2017, 2, 2);
+            var date2 = new DateTime(2017, 2, 2);
+
+            //act
+            dateRange.ValidateDates(date1, date2);
+        }
+
+        [TestMethod]
+        public void TestForNoExceptionForDifferentDates()
+        {
+            // arrange
+            DateRange dateRange = new DateRange();
+            var date1 = new DateTime(2017, 1, 2);
+            var date2 = new DateTime(2018, 7, 5);
+
+            //act
+            dateRange.ValidateDates(date1, date2);
+        }
+
+        [TestMethod]
+        public void EqualDatesGetDatesRangeTest()
+        {
+            // arrange
+            DateRange dateRange = new DateRange();
+            var date1 = new DateTime(2018, 12, 31);
+            var date2 = new DateTime(2018, 12, 31);
+
+            //act
+            string dateRangeStr = dateRange.GetDateRangeString(date1, date2);
+            Assert.AreEqual(dateRangeStr, "31.12.2018");
+        }
+
+        [TestMethod]
+        public void DifferentYearsGetDatesRangeTest()
+        {
+            // arrange
+            DateRange dateRange = new DateRange();
+            var date1 = new DateTime(2017, 6, 20);
+            var date2 = new DateTime(2018, 6, 20);
+
+            //act
+            string dateRangeStr = dateRange.GetDateRangeString(date1, date2);
+            Assert.AreEqual(dateRangeStr, "20.06.2017 - 20.06.2018");
+        }
+
+        [TestMethod]
+        public void DifferentMounthsGetDatesRangeTest()
+        {
+            // arrange
+            DateRange dateRange = new DateRange();
+            var date1 = new DateTime(45, 5, 5);
+            var date2 = new DateTime(45, 6, 5);
+
+            //act
+            string dateRangeStr = dateRange.GetDateRangeString(date1, date2);
+            Assert.AreEqual(dateRangeStr, "05.05 - 05.06.0045");
+        }
+
+        [TestMethod]
+        public void DifferentDaysGetDatesRangeTest()
+        {
+            // arrange
+            DateRange dateRange = new DateRange();
+            var date1 = new DateTime(557, 10, 5);
+            var date2 = new DateTime(557, 10, 17);
+
+            //act
+            string dateRangeStr = dateRange.GetDateRangeString(date1, date2);
+            Assert.AreEqual(dateRangeStr, "05 - 17.10.0557");
+        }
     }
 }
