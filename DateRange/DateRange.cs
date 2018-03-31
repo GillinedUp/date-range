906fe9f32ee6e8ecd1a546664e209ccbe79e1b78
GillinedUp <myrreah@gmail.com>
2018-03-31 12:25:55 +0300

    Add project files.

---
+++ DateRange/DateRange.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text;
+using System.Globalization;
+
+namespace DateRange
+{
+    class DateRange
+    {
+        readonly string format = "dd.MM.yyyy";
+
+        public void ValidateDates(DateTime date1, DateTime date2)
+        {
+            if (DateTime.Compare(date1, date2) > 0)
+                throw new ArgumentException("The first date is not earlier than the second.");
+        }
+
+        public DateTime[] ParseDates(string dateString1, string dateString2)
+        {
+            var provider = CultureInfo.InvariantCulture;
+            var date1 = DateTime.ParseExact(dateString1, format, provider);
+            var date2 = DateTime.ParseExact(dateString2, format, provider);
+            DateTime[] dates = { date1, date2 };
+            return dates;
+        }
+
+        public string GetDateRangeString(DateTime date1, DateTime date2)
+        {
+            StringBuilder dateStringBuilder = new StringBuilder();
+            if (date1.Year != date2.Year)
+                dateStringBuilder.Append(date1.ToString(format));
+            else if (date1.Month != date2.Month)
+                dateStringBuilder.Append(String.Format("{0:dd}.{0:MM}", date1));
+            else if (date1.Day != date2.Day)
+                dateStringBuilder.Append(String.Format("{0:dd}", date1));
+            if (DateTime.Compare(date1, date2) < 0)
+                dateStringBuilder.Append(" - ");
+            dateStringBuilder.Append(date2.ToString(format));
+            return dateStringBuilder.ToString();
+        }
+
+    }
+}
---
+++ DateRange/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace DateRange
+{
+    class Program
+    {
+        static void ExitProgram(string msg)
+        {
+            if (!(msg == ""))
+            {
+                Console.WriteLine(msg);
+            }
+            Console.WriteLine("Press any key...");
+            Console.ReadKey();
+            Environment.Exit(0);
+        }
+
+        static void Main(string[] args)
+        {
+            if (args.Length > 2 || args.Length < 2)
+            {
+                ExitProgram("Wrong number of arguments.");
+            }
+            string firstDateString = args[0];
+            string secondDateString = args[1];
+            DateRange dateRange = new DateRange();
+            try
+            {
+                var dates = dateRange.ParseDates(firstDateString, secondDateString);
+                var firstDate = dates[0];
+                var secondDate = dates[1];
+                dateRange.ValidateDates(firstDate, secondDate);
+                string dateRangeString = dateRange.GetDateRangeString(firstDate, secondDate);
+                Console.WriteLine(dateRangeString);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                ExitProgram("");
+            }
+        }
+    }
+}
